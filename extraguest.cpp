dfc87941ac17ead1d2b5e4821b20178dd0cd02bc
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-19 00:57:34 +0530

    Add extraguest.cpp for calculating total cost

---
+++ extraguest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    int total = X * 100 + (Y - X) * 150;
+
+    cout << total << endl;
+
+    return 0;
+}
