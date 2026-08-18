6b878c3dae7fe0c812012b9a3f1d8da2bb5b5c06
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-19 00:05:41 +0530

    Add program to check if number is divisible by 5

---
+++ seatedtogather.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X;
+    cin >> X;
+
+    if (X % 5 == 0)
+        cout << "No\n";
+    else
+        cout << "Yes\n";
+
+    return 0;
+}
