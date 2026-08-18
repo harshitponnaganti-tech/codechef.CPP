a926b1e9c72829e3efb89dac4fc33972b8de22f8
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-19 00:55:53 +0530

    Add main function to check specific characters

---
+++ favwords.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+
+    if (S[0] == 'c' || S[3] == 'f')
+        cout << "Yes";
+    else
+        cout << "No";
+
+    return 0;
+}
