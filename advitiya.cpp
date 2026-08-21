b8caf05be1bed8d794adb09d096f32571396ba7b
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-22 00:22:07 +0530

    Add main function to check age for ADVITIYA

---
+++ advitiya.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    if (N >= 16 && N <= 18)
+        cout << "ADVITIYA";
+    else
+        cout << "WAITING FOR ADVITIYA";
+
+    return 0;
+}
