c06ae748aefa3d5739ce0f46ebb9513acb6cd7b4
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-24 17:22:09 +0530

    Implement donation calculation in chefanddonation.cpp

---
+++ chefanddonation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << Y - X << "\n";
+    }
+
+    return 0;
+}
