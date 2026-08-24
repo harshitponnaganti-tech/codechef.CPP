f4f730420742f63c6749ac39790e58e06c7672cd
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-24 17:16:33 +0530

    Add battery health check program

---
+++ batteryhealth.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X >= 80)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
