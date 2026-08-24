d9a7a91277d789b83154557a0bcae373fc7c49d2
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-24 17:17:25 +0530

    Implement ageing calculation in ageing.cpp

---
+++ ageing.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        cout << X - 10 << "\n";
+    }
+
+    return 0;
+}
