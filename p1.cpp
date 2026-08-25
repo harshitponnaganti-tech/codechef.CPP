d45d1bec63e2f372923081993e5b161170152102
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 17:07:30 +0530

    Implement comparison between two integers

---
+++ p1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if (A <= B)
+        cout << "Tyro" << endl;
+    else
+        cout << "Dom" << endl;
+
+    return 0;
+}
