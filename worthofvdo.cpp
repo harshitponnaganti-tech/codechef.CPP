0d623e306755bf7610d7e8a89d05908122d85090
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 17:15:46 +0530

    Add worthofvdo.cpp to calculate video worth

---
+++ worthofvdo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int S;
+        cin >> S;
+
+        cout << S * 24 * 1000 << endl;
+    }
+
+    return 0;
+}
