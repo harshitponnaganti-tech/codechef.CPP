6f24b0bb3208531933a4a09f9e53f8a8f6ec62b3
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 17:09:26 +0530

    Implement time complexity check in timecomplexity.cpp

---
+++ timecomplexity.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X > Y)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
