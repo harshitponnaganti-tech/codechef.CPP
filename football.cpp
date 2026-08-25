c6c2b9ce9f99a0a3bd91c2122424089ea8e95e55
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 16:59:06 +0530

    Add football.cpp to determine kick type

---
+++ football.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int X, Y;
+    cin >> X >> Y;
+
+    if (X > Y)
+        cout << "FREEKICK" << endl;
+    else
+        cout << "PENALTY" << endl;
+
+    return 0;
+}
