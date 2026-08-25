e867b1dbb591e52c2284d37d09892038f1f6aa4b
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 16:49:47 +0530

    Implement episode check based on input value

---
+++ justoneepisode.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X;
+        cin >> X;
+
+        if (X > 24)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
