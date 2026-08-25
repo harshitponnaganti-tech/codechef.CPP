3f3d1f97236ca201acc7b27545c811b56dac79e5
harshitponnaganti-tech <harshitponnaganti@gmail.com>
2026-08-25 16:57:19 +0530

    Create manazone.cpp

---
+++ manazone.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        cout << Y / X << endl;
+    }
+
+    return 0;
+}
